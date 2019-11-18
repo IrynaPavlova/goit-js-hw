c4f126df6d01f96d10c3945206f29aa02092cf98
Pavlova <irochkapavlova85@gmail.com>
2019-11-18 15:53:39 +0200

    hw7

--- module-07/tasks.js
+++ module-07/tasks.js
@@ -7,14 +7,13 @@
 // console.log(`В списке ${list.length} категории`);
 
 // list.forEach(item => {
-//     const category = item.querySelector('h2').textContent;
-//     const quantity = item.querySelectorAll('ul li').length;
+//   const category = item.querySelector("h2").textContent;
+//   const quantity = item.querySelectorAll("ul li").length;
 
-//     console.log(`
+//   console.log(`
 //     Категория: ${category}
-//     Количество элементов: ${quantity}`)
-
-// })
+//     Количество элементов: ${quantity}`);
+// });
 
 /*-------------TASK#2-------------*/
 
@@ -43,57 +42,60 @@
 // const images = [
 //   {
 //     url:
-//       'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'White and Black Long Fur Cat',
+//       "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+//     alt: "White and Black Long Fur Cat"
 //   },
 //   {
 //     url:
-//       'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+//       "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+//     alt: "Orange and White Koi Fish Near Yellow Koi Fish"
 //   },
 //   {
 //     url:
-//       'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//     alt: 'Group of Horses Running',
-//   },
+//       "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+//     alt: "Group of Horses Running"
+//   }
 // ];
 
-// let string = '';
+// let string = "";
 
 // images.forEach(image => {
 //   string = `<li><img src="${image.url}" alt="${image.alt}" width='400px'<li>`;
-//   gallery.insertAdjacentHTML("afterbegin", string);
+//   gallery.insertAdjacentHTML("beforeend", string);
 // });
 
-// gallery.style.display = 'flex';
+// gallery.style.display = "flex";
 
 /*-------------TASK#4-------------*/
 
-// const counter = document.querySelector("#counter");
+// const refs = {
+//   increment: document.querySelector('[data-action="increment"]'),
+//   decrement: document.querySelector('[data-action="decrement"]'),
+//   result: document.querySelector("#value")
+// };
 
 // let counterValue = 0;
-// let result = document.querySelector("#value");
 
-// const increment = document.querySelector('[data-action="increment"]');
-// increment.addEventListener("click", () => {
+// refs.increment.addEventListener("click", () => {
 //   counterValue++;
-//   result.textContent = counterValue;
+//   refs.result.textContent = counterValue;
 // });
 
-// const decrement = document.querySelector('[data-action="decrement"]');
-// decrement.addEventListener("click", () => {
+// refs.decrement.addEventListener("click", () => {
 //   counterValue--;
-//   result.textContent = counterValue;
-// }); /////!!!!!!!!!!!!!!!!
+//   refs.result.textContent = counterValue;
+// });
 
 /*-------------TASK#5-------------*/
 
-// const input = document.querySelector('#name-input');
-// const output = document.querySelector('#name-output');
+// const refs = {
+//   input: document.querySelector("#name-input"),
+//   output: document.querySelector("#name-output")
+// };
 
-// input.addEventListener("input", (input) => {
-//   const element = input.currentTarget.value;
-//   output.textContent = element;
+// refs.input.addEventListener("input", event => {
+//   const element = event.currentTarget.value;
+//   refs.output.textContent = element;
 // });
 
 /*-------------TASK#6-------------*/
@@ -112,34 +114,39 @@
 
 /*-------------TASK#7-------------*/
 
-// const control = document.querySelector('#font-size-control[type=range]');
-// const text = document.querySelector('#text');
+// const refs = {
+//   control: document.querySelector("#font-size-control[type=range]"),
+//   text: document.querySelector("#text")
+// };
 
-// control.addEventListener("input", input => {
+// refs.control.addEventListener("input", input => {
 //   const value = input.currentTarget.value;
-//   text.style.fontSize = value + 'px';
+//   refs.text.style.fontSize = value + "px";
 // });
 
 /*-------------TASK#8-------------*/
 
-// const control = document.querySelector("#controls");
-// const render = document.querySelector('button[data-action="render"]');
-// const destroy = document.querySelector('button[data-action="destroy"]');
-// const boxes = document.querySelector("#boxes");
-// const fragment = document.createDocumentFragment();
+// const refs = {
+//   control: document.querySelector("#controls"),
+//   render: document.querySelector('button[data-action="render"]'),
+//   destroy: document.querySelector('button[data-action="destroy"]'),
+//   boxes: document.querySelector("#boxes"),
+//   fragment: document.createDocumentFragment()
+// };
 
 // const createBoxes = amount => {
 //   for (let i = 0; i < amount; i++) {
 //     let div = document.createElement("div");
-//     fragment.appendChild(div);
+//     refs.fragment.appendChild(div);
 //     let basicSize = 30;
 //     let size = basicSize + i * 10;
 //     div.style.cssText = `width: ${size}px; height: ${size}px; background-color: rgb(${getColor()}, ${getColor()}, ${getColor()} `;
-//   }
-//   boxes.appendChild(fragment);
+  
+// }
+//   refs.boxes.appendChild(refs.fragment);
 // };
 
-// render.addEventListener("click", () => {
+// refs.render.addEventListener("click", () => {
 //   let number = document.querySelector('input[type="number"]');
 //   let amount = number.value;
 //   return createBoxes(amount);
@@ -149,6 +156,6 @@
 //   return Math.floor(Math.random() * 256);
 // }
 
-// destroy.addEventListener("click", () => {
-//   boxes.innerHTML = "";
+// refs.destroy.addEventListener("click", () => {
+//   refs.boxes.innerHTML = "";
 // });
